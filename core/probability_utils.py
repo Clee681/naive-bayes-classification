91604559bd4c279fd52ea5ca5faaba73f74eec85
Suneel Chakravorty <suneel0101@gmail.com>
2015-02-22 13:47:18 -0500

    probability of class tested

--- core/probability_utils.py
+++ core/probability_utils.py
@@ -1,4 +1,18 @@
 def probability_of_feature(data, index):
     return (
-        sum([vector[0][index] for vector in data if vector[index]]) / float(len(data))
+        sum([
+            vector[0][index]
+            for vector in data
+            if vector[index]
+        ]) / float(len(data))
+    )
+
+
+def probability_of_class(data, class_label):
+    return (
+        len([
+            vector[1]
+            for vector in data
+            if vector[1] == class_label
+        ]) / float(len(data))
     )
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,18 +1,37 @@
 from sure import expect
 from core.matrix_utils import dot
-from core.probability_utils import probability_of_feature
+from core.probability_utils import (
+    probability_of_feature,
+    probability_of_class)
 
 def test_dot_product():
-    "Given two vectors, the dot product is calculated directly"
+    "Given two vectors, the dot product is calculated correctly"
     x = [0, 1, 1]
     y = [1, 2, 3]
     expect(dot(x, y)).to.equal(5)
+
 
 def test_probability_of_feature():
     "Given list of lists, the probability of feature1 is calculated"
     "['feature1', 'feature2']"
     data = [
-        [[1,0], True],
-        [[0,0], False]
+        [[1, 0], True],
+        [[0, 0], False]
     ]
     expect(probability_of_feature(data, 0)).to.equal(0.5)
+    expect(probability_of_feature(data, 1)).to.equal(0.0)
+
+
+def test_probability_of_success():
+    """
+    Given the training data, compute the probability of the
+    positive class.
+    """
+    data = [
+        [[1, 0], True],
+        [[0, 0], False],
+        [[0, 0], False],
+        [[0, 1], False]
+    ]
+    expect(probability_of_class(data, True)).to.equal(0.25)
+    expect(probability_of_class(data, False)).to.equal(0.75)
